2c45238e6da1486c1b4409c354289bf803051e92
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2020-08-18 09:57:56 +0700

    update footer row alamat

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -6,7 +6,11 @@
                     <h1 class="text-white font-16">CONTACT US</h1>
                     <h2 class="font-14 text-white">Head Office</h2>
                     <p>
-                        Ruby Commercial TD 19 Jl. Boulevard Selatan – Summarecon Bekasi 17142 Indonesia
+                        Ruby Commercial TD 19
+                        <br>
+                        Jl. Boulevard Selatan – Summarecon
+                        <br>
+                        Bekasi 17142 Indonesia
                     </p>
                     <h2 class="font-14 text-white">
                         Telepon
